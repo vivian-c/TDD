d001d679c86307c4667776385dd9ef1e2f1161e5
Vivian Chan <vchan@hearsaycorp.com>
2015-03-11 17:39:39 -0700

    Functional test now checks for a header

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,10 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
