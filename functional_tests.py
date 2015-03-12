ed700791a8fe11bb84417914bedd7c0790efb26e
Vivian Chan <vchan@hearsaycorp.com>
2015-03-11 18:09:01 -0700

    Added input element

--- functional_tests.py
+++ functional_tests.py
@@ -3,21 +3,24 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+		
+	def tearDown(self):
+		self.browser.quit()
 
-    def tearDown(self):
-        self.browser.quit()
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('To-Do', self.browser.title)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        self.fail('Finish the test!')
+		self.fail('Finish the test!')
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+	unittest.main(warnings='ignore')
